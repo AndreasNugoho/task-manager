f605f4d7824600326e514f28a25efc00c30e2833
Andreas Nugroho <andreas.nugroho@ti.ukdw.ac.id>
2025-01-14 03:47:25 +0700

    implement task management features with create, get, and list tasks (4341)

--- backend/src/routes/taskRoutes.js
+++ backend/src/routes/taskRoutes.js
@@ -1,10 +1,15 @@
 import express from "express";
+import {
+  createTask,
+  getTask,
+  getTasks,
+} from "../controllers/task/taskController.js";
+import { protect } from "../middleware/authMiddleware.js";
 
 const router = express.Router();
 
-router.post("task/create", (req, res) => {
-  //tes
-  res.send("Task created");
-});
+router.post("/task/create", protect, createTask);
+router.get("/tasks", protect, getTasks);
+router.get("/task/:id", protect, getTask);
 
 export default router;
